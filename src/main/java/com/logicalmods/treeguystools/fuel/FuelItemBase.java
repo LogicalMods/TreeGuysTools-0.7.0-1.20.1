d205f6bbc65b294146865b71ef4cfad54d362b11
LogicalMods <michael.regina@proton.me>
2024-06-16 20:54:15 -0400

    initial commit for v0.7.0 - 1.20.1

---
+++ src/main/java/com/logicalmods/treeguystools/fuel/FuelItemBase.java
@@ -0,0 +1,20 @@
+package com.logicalmods.treeguystools.fuel;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.jetbrains.annotations.Nullable;
+
+public class FuelItemBase extends Item {
+    private int burnTime;
+
+    public FuelItemBase(Properties pProperties, int burnTime) {
+        super(pProperties);
+        this.burnTime = burnTime;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return this.burnTime;
+    }
+}
